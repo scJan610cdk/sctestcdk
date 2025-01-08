34487b32a11e8f0cf92411a3af61d51d8d32eb81
scJan610cdk <schen6964@rogers.com>
2025-01-09 00:02:56 +0530

    update d3

---
+++ sc-ec2vpc-d3/lib/sc-ec2vpc-d3-stack.ts
@@ -0,0 +1,36 @@
+import * as cdk from 'aws-cdk-lib';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class scEc2VpcD3Stack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+
+    // usering default vpc
+    const vpc = ec2.Vpc.fromLookup(this,'scvpc',{
+        isDefault: true
+    });
+    // creating ec2 instance
+    const scvm = new ec2.Instance(this,'scvm1',{
+         vpc,
+         instanceType: new ec2.InstanceType('t2.micro'),
+         machineImage: new ec2.AmazonLinuxImage(),
+         keyPair: ec2.KeyPair.fromKeyPairName(this,'sckey','splunk-key'),
+        //       splunk-key is original key name of aws account
+        // so you have to use the same
+        instanceName: 'sc-linux-vm'
+ // above name of my linux machine 
+    });
+  }
+  // print 
+  new cdk.CfnOutput(this,'scIID',{
+      description: 'this will print instance id',
+      value: scvm.instanceId,
+  });
+  new cdk.CfnOutput(this,'scIN',{
+      description: 'print IN',  
+      value: scvm.instancePublicDnsName
+  });
+}
---
+++ sc-ec2vpc-d3/test/sc-ec2vpc-d3.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ScEc2VpcD3 from '../lib/sc-ec2vpc-d3-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/sc-ec2vpc-d3-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ScEc2VpcD3.ScEc2VpcD3Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
